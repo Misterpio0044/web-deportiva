2b7574ad17bd705f0c63f383f255b8146b7c2446
Mikel Pio <114400921+Misterpio0044@users.noreply.github.com>
2026-04-11 14:45:36 +0100

    repo(git): Configuración de validación de los commits

---
+++ commitlint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  /* Usamos la configuración convencional como base, la de feat, docs... */
+  extends: ['@commitlint/config-conventional'],
+  
+  /* Aquí van las reglas personalizadas */
+  rules: {
+    'type-enum': [
+      2, 
+      'always', 
+      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'repo']
+    ],
+    'subject-case': [2, 'always', 'sentence-case'],
+  },
+};
