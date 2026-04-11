4d81e53ea6ddd579b7639d930558e0da70318d76
Mikel Pio <pio.153922@e.unavarra.es>
2026-04-11 21:37:42 +0200

    repo(prettier): Instalar y configurar Prettier junto a ESLint. Close #6

--- commitlint.config.js
+++ commitlint.config.js
@@ -2,14 +2,10 @@
 module.exports = {
   /* Usamos la configuración convencional como base, la de feat, docs... */
   extends: ['@commitlint/config-conventional'],
-  
+
   /* Aquí van las reglas personalizadas */
   rules: {
-    'type-enum': [
-      2, 
-      'always', 
-      ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'repo']
-    ],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'style', 'refactor', 'test', 'repo']],
     'subject-case': [2, 'always', 'sentence-case'],
   },
 };
