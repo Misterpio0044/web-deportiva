8239bb681ab2d450ad750610468333b77a86439f
Mikel Pio <114400921+Misterpio0044@users.noreply.github.com>
2026-04-15 15:38:33 +0200

    fix(backend): Pequeño arreglo de la ruta de prueba

--- backend/src/infrastructure/server.ts
+++ backend/src/infrastructure/server.ts
@@ -7,7 +7,7 @@ const PORT = 3000;
 app.use(express.json());
 
 // Ruta de prueba
-app.get('/api/km', (req: Request, res: Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.status(200).json({ status: 'OK', message: 'OK' });
 });
 
